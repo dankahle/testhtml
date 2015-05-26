77fc34c928b11255670c44b48a17213e6f2ba9bb
Dan Kahle <dan.kahle@gmail.com>
2015-05-26 07:31:29 -0700

    finished rest-um eg (users/messages) hooked to node service using mongoose

--- zarch/rest-um/rest-um.js
+++ zarch/rest-um/rest-um.js
@@ -1,14 +1,13 @@
-
-(function() {
+(function () {
 
 	var app = angular.module('app', ['ngAnimate', 'restangular', 'ui.router', 'ct.ui.router.extras.sticky', 'ct.ui.router.extras.dsr']);
 
 	app.constant('$urlPaths', {
 		api: 'http://localhost:3000/api'
 	})
-	app.run(function(Restangular, $rootScope, $state, $stateParams, $location) {
-		Restangular.setErrorInterceptor(function(response, deferred, responseHandler) {
-			console.log('error:', response)
+	app.run(function (Restangular, $rootScope, $state, $stateParams, $location) {
+		Restangular.setErrorInterceptor(function (response, deferred, responseHandler) {
+			console.log('rest error:', response.data);
 			return true;
 		});
 
@@ -17,16 +16,19 @@
 		$rootScope.$location = $location;
 		$rootScope.log = console.log.bind(console);
 
-		$rootScope.$on('$stateChangeError', function(event, to, top, from, fromp, error) {
+		$rootScope.$on('$stateChangeError', function (event, to, top, from, fromp, error) {
 			console.log(error.stack);
 		})
-		$rootScope.$on('$stateChangeSuccess', function(event, to, top, from, fromp, error) {
+		$rootScope.$on('$stateChangeSuccess', function (event, to, top, from, fromp, error) {
 			console.log('>> ' + to.name);
 		})
 	})
-	app.config(function(RestangularProvider, $stateProvider, $urlRouterProvider, $urlPaths) {
+	app.config(function (RestangularProvider, $stateProvider, $urlRouterProvider, $urlPaths) {
 
 		RestangularProvider.setBaseUrl($urlPaths.api);
+		RestangularProvider.setRestangularFields({
+			id: '_id'
+		})
 
 		//$urlRouterProvider.otherwise()
 		$stateProvider
@@ -40,10 +42,10 @@
 			})
 			.state('user', {
 				abstract: true,
-				url: '/user/{userId:int}',
+				url: '/user/:userId',
 				template: '<ui-view></ui-view>',
 				resolve: {
-					user: function($users, $stateParams) {
+					user: function ($users, $stateParams) {
 						return $users.getOne($stateParams.userId);
 					}
 				}
@@ -64,43 +66,44 @@
 				templateUrl: 'messageTemp'
 			})
 			.state('user.message.edit', {
-				url: '/{messageId:int}/edit',
+				url: '/:messageId/edit',
 				resolve: {
-					message: function(user, $stateParams) {
+					message: function (user, $stateParams) {
 						return user.one('messages', $stateParams.messageId).get();
 					}
 				},
 				controller: 'messageEditCtrl',
 				templateUrl: 'messageTemp'
 			})
+
 	})
 
 	app.controller('bodyCtrl', function ($scope, Restangular, $users) {
 		$scope.users = [];
 
-		$scope.refreshUsers =  refreshUsers = function() {
-			$users.getAll().then(function(users) {
+		$scope.refreshUsers = refreshUsers = function () {
+			$users.getAll().then(function (users) {
 
 				// remove from scope.users what's not in users
-				$scope.users.forEach(function(v) {
-					if(!_.find(users, {id: v.id}))
+				$scope.users.forEach(function (v) {
+					if (!_.find(users, {_id: v._id}))
 						_.remove($scope.users, v);
 				});
 				// update data for existing users
-				$scope.users.forEach(function(v) {
-					var user = _.find(users, {id: v.id})
+				$scope.users.forEach(function (v) {
+					var user = _.find(users, {_id: v._id})
 					v.name = user.name;
 					v.age = user.age;
 				});
 				// add what's in users and not in scope.users
-				users.forEach(function(v) {
-					if(!_.find($scope.users, {id: v.id}))
+				users.forEach(function (v) {
+					if (!_.find($scope.users, {_id: v._id}))
 						$scope.users.push(v);
 				});
-				$scope.users.sort(function(a,b) {
-					if(a.name < b.name)
+				$scope.users.sort(function (a, b) {
+					if (a.name < b.name)
 						return -1;
-					if(a.name > b.name)
+					if (a.name > b.name)
 						return 1;
 					return 0;
 				});
@@ -110,8 +113,8 @@
 			})
 		};
 
-		$scope.removeUser = function(user) {
-			$users.remove(user).then(function() {
+		$scope.removeUser = function (user) {
+			$users.remove(user).then(function () {
 				refreshUsers();
 			})
 		}
@@ -119,36 +122,40 @@
 		refreshUsers()
 	})
 
-	app.controller('userAddCtrl', function($scope, $users) {
+	app.controller('userAddCtrl', function ($scope, $users) {
 		$scope.mode = 'add';
-		$scope.submit = function() {
-			$users.add($scope.user).then(function() {
+		$scope.submit = function () {
+			$users.add($scope.user).then(function () {
 				$scope.refreshUsers();
+			}, function(err) {
+				alert('Failed to add user.');
 			})
 		}
 		$('#user-name').focus()
 	})
 
-	app.controller('userEditCtrl', function($scope, user, $users) {
+	app.controller('userEditCtrl', function ($scope, user, $users) {
 		$scope.mode = 'edit';
 		$scope.user = user;
-		$scope.submit = function() {
-			$users.update(user).then(function() {
+		$scope.submit = function () {
+			$users.update(user).then(function (resp) {
 				$scope.refreshUsers();
+			}, function(err) {
+				alert('Failed to update user.')
 			})
 		}
 		$('#user-name').focus()
 	})
-	app.controller('messagesCtrl', function($scope, user, $users) {
+	app.controller('messagesCtrl', function ($scope, user, $users) {
 
-		$scope.refreshMessages = refreshMessages = function() {
-			user.getList('messages').then(function(messages) {
+		$scope.refreshMessages = refreshMessages = function () {
+			user.getList('messages').then(function (messages) {
 				$scope.messages = messages.reverse();
 			})
 		}
 
-		$scope.removeMessage = function(message) {
-			$users.removeMessage(message).then(function() {
+		$scope.removeMessage = function (message) {
+			$users.removeMessage(message).then(function () {
 				_.remove($scope.messages, message);
 			})
 		}
@@ -156,27 +163,26 @@
 		refreshMessages();
 	})
 
-	app.controller('messageAddCtrl', function($scope, user, $users) {
+	app.controller('messageAddCtrl', function ($scope, user, $users) {
 		$scope.mode = 'Add';
-		$scope.submit = function() {
-			$users.addMessage(user, $scope.message).then(function(message) {
+		$scope.submit = function () {
+			$users.addMessage(user, $scope.message).then(function (message) {
 				$scope.messages.unshift(message)
 			})
 		}
 		$('#message').focus()
 	})
 
-	app.controller('messageEditCtrl', function($scope, $users, user, message) {
+	app.controller('messageEditCtrl', function ($scope, $users, user, message) {
 		$scope.mode = 'Edit';
 		$scope.message = message;
-		$scope.submit = function() {
-			$users.updateMessage(message).then(function() {
+		$scope.submit = function () {
+			$users.updateMessage(message).then(function () {
 				$scope.refreshMessages();
 			})
 		}
 		$('#message').focus()
 	})
-
 
 
 })() // iife
